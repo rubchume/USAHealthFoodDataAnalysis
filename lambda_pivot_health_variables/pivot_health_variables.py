0676c103101f3b8924fd2ab289319943bcf5324c
Rubén Chuliá Mena <rchulia@eturia.es>
2022-10-26 17:15:18 -0500

    Now it works!! Lambda responds!!

--- lambda_pivot_health_variables/pivot_health_variables.py
+++ lambda_pivot_health_variables/pivot_health_variables.py
@@ -1,13 +1,6 @@
+import json
+
+
 def pivot_variables(event, context):
-    query_string_parameters = event['queryStringParameters']
     print(event)
-    print(query_string_parameters)
-    return {
-        "statusCode": 200,
-        "headers": {
-          'Content-Type': 'application/json',
-        },
-        "body": {
-          "message": f"this is the response. This is the event: {event}",
-        },
-    }
+    return {"statusCode": 200, "body": json.dumps(event["queryStringParameters"])}
